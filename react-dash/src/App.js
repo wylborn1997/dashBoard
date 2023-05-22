bb917d2393f04c29f9103f49464fc0cf9d992d5f
YalingWang <yaling_wang@human-horizons.com>
2023-05-22 14:19:42 +0800

    Feat: 新增热更新配置

--- react-dash/src/App.js
+++ react-dash/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { hot } from "react-hot-loader/root";
+import "./App.css";
+import { useState } from "react";
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function handelClick() {
+    console.log("你居然敢点我");
+    setCount(count + 2);
+    // console.log(count);
+  }
+  return (
+    <div>
+      <button onClick={handelClick}>点我试试{count + 2}</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MyButton></MyButton>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,4 +39,4 @@ function App() {
   );
 }
 
-export default App;
+export default hot(App);
