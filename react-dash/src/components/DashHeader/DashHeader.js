c3265e666a4753cdfe028b66203bb3aa217d0f2e
YalingWang <yaling_wang@human-horizons.com>
2023-07-03 16:23:26 +0800

    Feat: header

--- react-dash/src/components/DashHeader/DashHeader.js
+++ react-dash/src/components/DashHeader/DashHeader.js
@@ -1,16 +1,22 @@
-import {  Layout, theme } from 'antd';
+import { Layout, theme } from 'antd';
+import "./DashHeader.less";
 const { Header } = Layout;
 function DashHeader (params) {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const headerTitle = '空桑发展报告'
   return (
     <Header
+      class="title"
           style={{
             padding: 0,
             background: colorBgContainer,
           }}
-        />
+    >
+      {headerTitle}
+        </Header>
   )
 }
 export default DashHeader
